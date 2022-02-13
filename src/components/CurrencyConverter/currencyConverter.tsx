df9d9440e6026b7de1af0e98c19d50d66060bf73
alexReactDev <alexander.work.mailbox@gmail.com>
2022-02-13 20:30:06 +0800

    Added CurrencyConverter Dummy

---
+++ src/components/CurrencyConverter/currencyConverter.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+interface IProps {
+	value: number
+}
+
+const CurrencyConverter: FC<IProps> = ({ value }) => {
+	console.log(value);
+	return(
+		<>
+		{`$ ${value.toFixed(2)}`}
+		</>
+	)
+}
+
+export default CurrencyConverter;
