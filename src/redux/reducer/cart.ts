9444305edfd23c2d9fdbbc3578a4ee253eb8db49
alexReactDev <alexander.work.mailbox@gmail.com>
2022-02-13 20:24:39 +0800

    Added cart reducer

---
+++ src/redux/reducer/cart.ts
@@ -0,0 +1,52 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IState {
+	total: number,
+	products: {
+		[key: string]: number
+	}
+}
+
+interface IPayload {
+	productId: string,
+	productPrice: number
+}
+
+interface IPayloadWithAmount extends IPayload {
+	amount: number
+}
+
+const initialState: IState = {
+	total: 0,
+	products: {}
+}
+
+const cartSlice = createSlice({
+	name: 'cart',
+	initialState,
+	reducers: {
+		productIncrement(state, action: PayloadAction<IPayloadWithAmount>) {
+			const { productId, productPrice, amount } = action.payload;
+
+			state.products[productId] = state.products[productId] | 0 + amount;
+			state.total = state.total + productPrice * amount;
+		},
+		productDecrement(state, action: PayloadAction<IPayloadWithAmount>) {
+			const { productId, productPrice, amount } = action.payload;
+
+			state.total = state.total - productPrice * amount > state.products[productId] ? state.products[productId] : amount;
+			state.products[productId] = state.products[productId] - amount;
+			if(state.products[productId] <= 0) delete state.products[productId];
+		},
+		removeProduct(state, action: PayloadAction<IPayload>) {
+			const { productId, productPrice } = action.payload;
+
+			state.total = state.total - productPrice * state.products[productId];
+			delete state.products[productId];
+		}
+	}
+})
+
+export default cartSlice.reducer;
+
+export const {productIncrement, productDecrement, removeProduct} = cartSlice.actions;
--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -2,8 +2,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { connectRouter } from "connected-react-router";
 import history from "../../history";
 
+import cart from "./cart";
+
 const rootReducer = combineReducers({
 	router: connectRouter(history),
+	cart,
 })
 
 export default rootReducer;
