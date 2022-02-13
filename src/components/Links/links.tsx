71c60428e05d5da96669e2bc8a7c971e07250bb6
alexReactDev <alexander.work.mailbox@gmail.com>
2022-02-13 18:20:45 +0800

    Added Links component

---
+++ src/components/Links/links.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import style from './links.module.scss';
+
+interface IProps {
+	className?: string,
+	links: {
+		link: string,
+		text: string
+	}[]
+}
+
+const Links: FC<IProps> = ({ className='', links }) => {
+	return(
+		<ul className={`${className} ${style.links}`}>
+			{
+				links.map((link) => {
+					return(
+						<li className={style.links__link} key={link.link} >
+							<Link className="nav-link" to={link.link}>
+								{link.text}
+							</Link>
+						</li>
+					)
+				})
+			}
+		</ul>
+	)
+}
+
+export default Links;
