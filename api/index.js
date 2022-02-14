afdcc702e8fde445fbc06ac861416655a92c0b31
alexReactDev <alexander.work.mailbox@gmail.com>
2022-02-14 22:23:42 +0800

    Updated api (Added pagination to categories)

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const router = require("./router");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
--- api/router/index.js
+++ api/router/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const fixtures = require("../fixtures.json");
+const fixtures = require("../fixtures2.json");
 
 const router = new express.Router();
 
 router.get("/categories/:category", (req, res, next) => {
-	
-	const category = req.params.category;
 
-	if(fixtures.categories[category]) {
-		res.send(fixtures.categories[category]);
+	const reqCategory = req.params.category;
+	const page = +req.query.page || 1;
+
+	if(fixtures.categories[reqCategory]) {
+		const category = fixtures.categories[reqCategory];
+		const pageLength = process.env.PAGE_LENGTH;
+		const totalPages = Math.ceil(category.length / pageLength);
+		const data = category.slice(pageLength * page - pageLength, pageLength * page);
+
+		res.send({
+			page,
+			totalPages,
+			data,
+		});
 	}
 
 	next();
