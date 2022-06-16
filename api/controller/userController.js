d1beac1108d2781acb71123bf19a3aebc2bdb3d4
alexReactDev <alexander.work.mailbox@gmail.com>
2022-06-16 23:00:04 +0800

    Connected datacase to userController

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,36 +1,236 @@
-const fixtures = require("../fixtures4.2.json");
-
+const db = require("../db");
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 class UserController {
-	getUser(req, res) {
-		res.status(200).json(fixtures.userdata);
+	async getUser(req, res) {
+		const userId = req.params.userId;
+
+		let user;
+
+		try {
+			user = (await db.query("SELECT * FROM users where id = $1;", [userId])).rows[0];
+		}
+		catch(e) {
+			return res.sendStatus(500)
+		}
+
+		if(!user) return res.sendStatus(404);
+
+		res.send(user);
 	}
 
-	createUser(req, res) {
-		res.status(201).json(req.body);
+	async createUser(req, res) {
+		const {payload} = req.body;
+		const userId = uuid.v4();
+
+		let createdUser;
+
+		try {
+			createdUser = (await db.query("INSERT INTO users (id, name, surname, email, phone, town, street, house, apartment, zip) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;",
+			[userId, payload.name, payload.surname, payload.email, payload.phone, payload.town, payload.street, payload.house, payload.apartment, payload.zip])).rows[0];
+		}
+		catch(e) {
+			console.log(e);
+			return res.sendStatus(500);
+		}
+
+		try {
+			const hashPassword = await bcrypt.hash(payload.password, +process.env.BCRYPT_SALT_ROUNDS);
+		
+			await(db.query("INSERT INTO users_passwords (user_id, password) values($1, $2);", [createdUser.id, hashPassword]));	
+		}
+		catch(e) {
+			console.log(e);
+			return res.sendStatus(500);
+		}
+
+		res.status(201).send(createdUser);
 	}
 
-	changeUserProfile(req, res) {
-		res.status(200).json({
-			...req.body,
-			email: fixtures.userdata.email
-		});
+	async changeUserProfile(req, res) {
+		const userId = req.params.userId;
+		const {password, payload} = req.body;
+		
+		let user;
+
+		try {
+			user = (await db.query("SELECT * FROM users where id = $1;", [userId])).rows[0];
+			if(!user) return res.sendStatus(400);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}	
+
+		try {
+			const hashPassword = (await db.query("SELECT password FROM users_passwords where user_id = $1;", [user.id])).rows[0].password;
+
+			const isCorrect = await bcrypt.compare(password, hashPassword);
+			
+			if(!isCorrect) return res.sendStatus(403);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		let updatedProfile;
+
+		try {
+			updatedProfile = (await db.query("UPDATE users SET (name, surname, phone, town, street, house, apartment, zip) = ($1, $2, $3, $4, $5, $6, $7, $8) where id = $9 RETURNING *;", 
+			[payload.name, payload.surname, payload.phone, payload.town, payload.street, payload.house, payload.apartment, payload.zip, user.id])).rows[0];
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		res.send(updatedProfile);
 	}
 
-	changeUserEmail(req, res) {
-		const {email} = req.body;
+	async changeUserEmail(req, res) {
+		const userId = req.params.userId;
+		const {password, payload} = req.body;
 
-		res.status(200).json({
-			...fixtures.userdata,
-			email,
-		});
+		let user;
+
+		try {
+			user = (await db.query("SELECT * FROM users where id = $1;", [userId])).rows[0];
+			if(!user) return res.sendStatus(400);
+		}
+		catch(e) {
+			res.sendStatus(500);
+		}
+
+		try {
+			const hashPassword = (await db.query("SELECT password FROM users_passwords where user_id = $1;", [user.id])).rows[0].password;
+
+			const isCorrect = await bcrypt.compare(password, hashPassword);
+			
+			if(!isCorrect) return res.sendStatus(403);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		let updatedEmail;
+
+		try {
+			updatedEmail = (await db.query("UPDATE users SET email = $1 where id = $2 RETURNING email;", [payload, user.id])).rows[0].email;
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		res.send(updatedEmail);
 	}
 
-	changeUserPassword(req, res) {
+	async changeUserPassword(req, res) {
+		const userId = req.params.userId;
+		const {password, payload} = req.body;
+
+		let user; 
+
+		try {
+			const user = (await db.query("SELECT * FROM users where id = $1;", [userId])).rows[0];
+			if(!user) return res.sendStatus(400);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		try {
+			const hashPassword = (await db.query("SELECT password FROM users_passwords where user_id = $1;", [user.id])).rows[0].password;
+
+			const isCorrect = await bcrypt.compare(password, hashPassword);
+			
+			if(!isCorrect) return res.sendStatus(403);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		try {
+			const newHashPassword = await bcrypt.hash(payload, +process.env.BCRYPT_SALT_ROUNDS);
+
+			await db.query("UPDATE users_passwords SET password = $1 where user_id = $2 RETURNING password;", [newHashPassword, user.id]);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+		
 		res.sendStatus(200);
 	}
 
-	deleteUser(req, res) {
+	async deleteUser(req, res) {
+		const userId = req.params.userId;
+		const {password} = req.body;
+
+		let user;
+
+		try {
+			user = (await db.query("SELECT * FROM users where id = $1;", [userId])).rows[0];
+			if(!user) res.sendStatus(400);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		try {
+			const hashPassword = (await db.query("SELECT password FROM users_passwords where user_id = $1;", [user.id])).rows[0].password;
+
+			const isCorrect = await bcrypt.compare(password, hashPassword);
+			
+			if(!isCorrect) return res.sendStatus(403);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		try {
+			await db.query("DELETE FROM cart_products where user_id = $1;", [user.id]);
+			await db.query("DELETE FROM wishlist_products where user_id = $1;", [user.id]);
+			await db.query("DELETE FROM users_passwords where user_id = $1;", [user.id]);
+			await db.query("DELETE FROM users_preferences where user_id = $1;", [user.id]);
+			await db.query("DELETE FROM users_orders where user_id = $1;", [user.id]);
+			await db.query("DELETE FROM users where id = $1;", [user.id]);
+		}
+		catch(e) {
+			res.sendStatus(500);
+		}
+
 		res.sendStatus(200);
+	}
+
+	async createTemporaryUser() {
+		const userId = "T-" + uuid.v4();
+		const expires = new Date().getTime() + +process.env.SESSION_EXPIRES_IN;
+
+		let createdUser;
+
+		try {
+			createdUser = (await db.query("INSERT INTO temporary_users (id, expires) values($1, $2) RETURNING *;",
+			[userId, expires])).rows[0];
+		}
+		catch(e) {
+			console.log(e);
+			throw new Error(e);
+		}
+
+		return createdUser;
+	}
+
+	async updateTemporaryUser(userId) {
+		const newExpires = new Date().getTime() + +process.env.SESSION_EXPIRES_IN;
+
+		let updatedUser;
+
+		try {
+			updatedUser = (await db.query("UPDATE temporary_users SET expires = $1 where id = $2 RETURNING *;", [newExpires, userId])).rows[0];
+		}
+		catch(e) {
+			console.log(e);
+			throw new Error(e);
+		}
+
+		return updatedUser;
 	}
 }
 
--- api/router/userRouter.js
+++ api/router/userRouter.js
@@ -1,14 +1,14 @@
 const Router = require("express").Router;
-const { getUser, createUser, changeUserProfile, changeUserEmail, changeUserPassword, deleteUser, getUserOrders, getUserPreferences } = require("../controller/userController");
+const { getUser, createUser, changeUserProfile, changeUserEmail, changeUserPassword, deleteUser } = require("../controller/userController");
 
 
 const router = new Router();
 
 router.get("/:userId", getUser);
 router.post("/", createUser);
-router.patch("/profile", changeUserProfile);
-router.patch("/email", changeUserEmail);
-router.patch("/password", changeUserPassword);
+router.put("/profile/:userId", changeUserProfile);
+router.put("/email/:userId", changeUserEmail);
+router.put("/password/:userId", changeUserPassword);
 router.delete("/:userId", deleteUser);
 
 module.exports = router;
