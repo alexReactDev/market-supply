b73e6057702a25524d2c2dcec2c0a6e00d9aa4fb
alexReactDev <alexander.work.mailbox@gmail.com>
2022-06-18 23:32:43 +0800

    Changed authorization controller according db changes

--- api/controller/authorizationController.js
+++ api/controller/authorizationController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 class authorizationController {
 	async login(req, res) {
+		const previousUserTokenData = req.tokenData;
 		const { password, payload: email } = req.body;
 
 		let user;
@@ -29,9 +30,58 @@ class authorizationController {
 
 		if(!isPasswordCorrect) return res.status(403).send("Incorrect password");
 
+		let userPersonId;
+
+		try {
+			userPersonId = (await db.query("SELECT id FROM persons where user_id = $1;", [user.id])).rows[0].id;
+		}
+		catch(e) {
+			console.log(e);
+			res.sendStatus(500);
+		}
+
+		if(previousUserTokenData && !previousUserTokenData.authorized) {
+			try {
+				const tempUserCartProducts = (await db.query("SELECT * FROM cart_products where person_id = $1;", [previousUserTokenData.personId])).rows;
+
+				tempUserCartProducts.forEach(async (tempUserCartProduct) => {
+					const userCartProduct = (await db.query("SELECT * FROM cart_products where person_id = $1;", [userPersonId])).rows[0];
+
+					if(userCartProduct) {
+						const sum = tempUserCartProduct.amount + userCartProduct.amount;
+						await db.query("UPDATE cart_products SET amount = $1 where person_id = $2;", [sum, userPersonId]);
+					}
+					else {
+						await db.query("INSERT INTO cart_products (person_id, product_id, amount) values($1, $2, $3);", [userPersonId, tempUserCartProduct.product_id, tempUserCartProduct.amount]);
+					}
+				})
+			}
+			catch(e) {
+				console.log(e);
+				res.sendStatus(500);
+			}
+
+			try {
+				const tempUserWishlistProducts = (await db.query("SELECT * FROM wishlist_products where person_id = $1;", [previousUserTokenData.personId])).rows;
+
+				tempUserWishlistProducts.forEach(async (tempUserWishlistProduct) => {
+					const userWishlistProduct = (await db.query("SELECT * FROM wishlist_products where person_id = $1;", [userPersonId])).rows[0];
+
+					if(!userWishlistProduct) {
+						await db.query("INSERT INTO wishlist_products (person_id, product_id) values($1, $2);", [userPersonId, tempUserWishlistProduct.product_id]);
+					}
+				})
+			}
+			catch(e) {
+				console.log(e);
+				res.sendStatus(500);
+			}
+		}
+
 		const tokenData = {
 			authorized: true,
-			userId: user.id
+			userId: user.id,
+			personId: userPersonId
 		}
 
 		const userToken = jwt.sign({data: tokenData}, process.env.JWT_SECRET, {expiresIn: +process.env.SESSION_EXPIRES_IN / 1000});
--- api/router/authorizationRouter.js
+++ api/router/authorizationRouter.js
@@ -1,10 +1,11 @@
 const { login, logout } = require("../controller/authorizationController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const Router = require("express").Router;
 
 const router = new Router();
 
-router.post("/login", login);
+router.post("/login", authMiddleware(true), login);
 router.post("/logout", logout);
 
 module.exports = router;
