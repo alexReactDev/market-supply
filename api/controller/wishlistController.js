0f8fee8ad22c2b9510adb915e163a1f9b9f262c2
alexReactDev <alexander.work.mailbox@gmail.com>
2022-06-25 17:40:56 +0800

    Rewrote wishlistController according to db and authorization changes

--- api/controller/wishlistController.js
+++ api/controller/wishlistController.js
@@ -1,24 +1,71 @@
-const fixtures = require("../fixtures4.2.json");
+const db = require("../db");
 
 class WishlistController {
-	getWishlistItems(req, res) {
-		res.json(fixtures.wishlist);
+	async getWishlistItems(req, res) {
+		const personId = req.tokenData.personId;
+
+		let items;
+
+		try {
+			items = (await db.query("SELECT * FROM wishlist_products where person_id = $1;", [personId])).rows.map((item) => item.product_id);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		res.send(items);
 	}
 
-	addWishlistItem(req, res) {
-		const {id} = req.body;
+	async addWishlistItem(req, res) {
+		const personId = req.tokenData.personId;
+		const productId = req.params.productId;
 
-		res.status(202).json(id);
+		let product;
+
+		try {
+			product = (await db.query("SELECT * FROM products where id = $1;", [productId])).rows[0];
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		if(!product) return res.sendStatus(400);
+
+		try {
+			await db.query("INSERT INTO wishlist_products (person_id, product_id) values ($1, $2);", [personId, product.id]);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		this.getWishlistItems(req, res);
 	}
 
-	removeWishlistItem(req, res) {
-		const id = req.params.productId;
+	async removeWishlistItem(req, res) {
+		const personId = req.tokenData.personId;
+		const productId = req.params.productId;
 
-		res.json(id);
+		try {
+			await db.query("DELETE FROM wishlist_products where person_id = $1 and product_id = $2;", [personId, productId]);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		this.getWishlistItems(req, res);
 	}
 
-	clearWishlist(req, res) {
-		res.sendStatus(200);
+	async clearWishlist(req, res) {
+		const personId = req.tokenData.personId;
+
+		try {
+			await db.query("DELETE FROM wishlist_products where person_id = $1;", [personId]);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		this.getWishlistItems(req, res);
 	}
 }
 
