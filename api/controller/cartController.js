52b450e9455dd9e882da692ef7f37e24f7421e82
alexReactDev <alexander.work.mailbox@gmail.com>
2022-06-25 16:50:35 +0800

    Rewrote cart Controller in accordance with db and authorization changes

--- api/controller/cartController.js
+++ api/controller/cartController.js
@@ -1,45 +1,125 @@
-const fixtures = require("../fixtures4.2.json");
-
+const db = require("../db");
 class CartController {
-	getCartItems(req, res) {
-		let total = 0;
+	async getCartItems(req, res) {
+		const personId = req.tokenData.personId;
+		
+		let items;
 
-		Array.from(Object.keys(fixtures.cart)).forEach((productId) => {
-			total += fixtures.products[productId].price * fixtures.cart[productId];
-		})
+		try {
+			items = (await db.query("SELECT * FROM cart_products where person_id = $1;", [personId])).rows;
+		}
+		catch(e) {
+			console.log(e);
+			return res.sendStatus(500);
+		}
 
-		res.json({
-			total,
-			products: fixtures.cart
-		});
+		const cart = {};
+
+		items.forEach((item) => cart[item.product_id] = item.amount);
+
+		res.send(cart);
 	}
 
-	cartItemIncrement(req, res) {
-		const {id, amount} = req.body;
+	async cartItemIncrement(req, res) {
+		const personId = req.tokenData.personId;
+		const productId = req.params.productId;
+		const { payload: amount } = req.body;
 
-		res.json({
-			id,
-			amount
-		});
+		let product;
+
+		try {
+			product = (await db.query("SELECT * FROM products where id = $1;", [productId])).rows[0];
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		if(!product) return res.sendStatus(400);
+
+		let cartItem;
+
+		try {
+			cartItem = (await db.query("SELECT * FROM cart_products where person_id = $1 and product_id = $2;", [personId, productId])).rows[0];
+		}
+		catch(e) {
+			console.log(e);
+			return res.sendStatus(500);
+		}
+
+		try {
+			if(cartItem) {
+				await db.query("UPDATE cart_products SET amount = $1 where person_id = $2 and product_id = $3;", [cartItem.amount + amount, personId, cartItem.product_id]);
+			}
+			else {
+				await db.query("INSERT INTO cart_products (person_id, product_id, amount) values ($1, $2, $3);", [personId, productId, amount]);
+			}
+		}
+		catch(e) {
+			console.log(e);
+			return res.sendStatus(500);
+		}
+
+		this.getCartItems(req, res);
 	}
 
-	cartItemDecrement(req, res) {
-		const {id, amount} = req.body;
+	async cartItemDecrement(req, res) {
+		const personId = req.tokenData.personId;
+		const product = req.params.productId;
+		const { payload: amount } = req.body;
 
-		res.json({
-			id,
-			amount
-		});
+		let cartItem;
+
+		try {
+			cartItem = (await db.query("SELECT * FROM cart_products where person_id = $1 and product_id = $2;", [personId, product])).rows[0];
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		if(!cartItem) return res.sendStatus(400);
+
+		let newAmount = cartItem.amount - amount;
+
+		try {
+			if(newAmount <= 0) {
+				await (db.query("DELETE FROM cart_products where person_id = $1 and product_id = $2;", [personId, product]));
+			}
+			else {
+				await (db.query("UPDATE cart_products SET amount = $1 where person_id = $2 and product_id = $3;", [newAmount, personId, product]));
+			}
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		this.getCartItems(req, res);
 	}
 
-	removeCartItem(req, res) {
-		const id = req.params.productId;
+	async removeCartItem(req, res) {
+		const personId = req.tokenData.personId;
+		const product = req.params.productId;
 
-		res.json(id);
+		try {
+			await db.query("DELETE FROM cart_products where person_id = $1 and product_id = $2;", [personId, product]);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		this.getCartItems(req, res);
 	}
 
-	emptyCart(req, res) {
-		res.sendStatus(200);
+	async emptyCart(req, res) {
+		const personId = req.tokenData.personId;
+
+		try {
+			await db.query("DELETE FROM cart_products where person_id = $1;", [personId]);
+		}
+		catch(e) {
+			return res.sendStatus(500);
+		}
+
+		this.getCartItems(req, res);
 	}
 }
 
