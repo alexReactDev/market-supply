8b0c9ede43f9ec43a2ee8b0c389c105605348bb9
alexReactDev <alexander.work.mailbox@gmail.com>
2022-07-19 16:43:43 +0800

    Changed topNav

--- src/components/TopNav/topNav.tsx
+++ src/components/TopNav/topNav.tsx
@@ -1,5 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../hooks";
+import { collectionsSelector, foldersWithItemsSelector } from "../../redux/selectors";
 import style from './topNav.module.scss';
 
 interface IProps {
@@ -7,55 +9,77 @@ interface IProps {
 }
 
 const TopNav: FC<IProps> = ({ className='' }) => {
+
+	const folders = useAppSelector(foldersWithItemsSelector);
+	const collections = useAppSelector(collectionsSelector);
+
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
 		<nav className={`${className} ${style.navbar}`}>
 			<div className='content'>
 				<ul className={style.navbar__items}>
-					<Link className="nav-link" to='/categories/clothing'>
-						<li className={style.navbar__item}>
-								CLOTHING
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/electronics'>
-						<li className={style.navbar__item}>
-							ELECTRONICS
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/shoes'>
-						<li className={style.navbar__item}>
-							SHOES
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/watches'>
-						<li className={style.navbar__item}>
-							WATCHES
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/jewellery'>
-						<li className={style.navbar__item}>
-							JEWELLERY
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/health-and-beauty'>
-						<li className={style.navbar__item}>
-							{"HEALTH&BEAUTY"}
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/kids-and-babies'>
-						<li className={style.navbar__item}>
-							{"KIDS&BABIES"}
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/sports'>
-						<li className={style.navbar__item}>
-							SPORTS
-						</li>
-					</Link>
-					<Link className="nav-link" to='/categories/home-and-garden'>
-						<li className={style.navbar__item}>
-							{"HOME&GARDEN"}
-						</li>
-					</Link>
+					<li className={`${style.navbar__hiddenMenu} ${style.hiddenMenu}`}>
+						<div className={`${style.hiddenMenu__modal} ${menuOpen ? style.hiddenMenu__modal_visible : ""}`} onClick={() => setMenuOpen(false)} />
+						<div className={`${style.hiddenMenu__burger} ${menuOpen ? style.hiddenMenu__burger_active : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+							<span />
+						</div>
+						<div className={`${style.hiddenMenu__body} ${menuOpen ? style.hiddenMenu__body_visible : ""}`}>
+							{
+								folders.map((folder) => {
+									return(
+										<ul key={folder.id} className={style.hiddenMenu__folder}>
+											<p className={style.hiddenMenu__title}>
+												{folder.name}
+											</p>
+											{
+												folder.items.map((cat) => {
+													return(
+														<li key={cat.id} className={style.hiddenMenu__category}>
+															<Link to={`/categories/${folder.url_name}/${cat.url_name}`} className={`${style.hiddenMenu__link} nav-link`} >
+																{cat.name}
+															</Link>
+														</li>
+													)
+												})
+											}
+										</ul>
+									)
+								})
+							}
+							<ul className={style.hiddenMenu__folder}>
+								<p className={style.hiddenMenu__title}>
+									Special	 
+								</p>
+								{
+									Array.from(Object.keys(collections)).map((col) => {
+										const collection = collections[col];
+
+										return(
+											<li className={style.hiddenMenu__category}>
+												<Link to={`/collections/${collection.url_name}`} className={`${style.hiddenMenu__link} nav-link`} >
+													{collection.name}
+												</Link>
+											</li>
+										)
+									})
+								}
+							</ul>
+						</div>
+					</li>
+					{
+						Array.from(Object.keys(collections)).map((col) => {
+							const collection = collections[col];
+
+							return(
+								<Link to={`/collections/${collection.url_name}`} className={style.navbar__item}>
+									<li>
+										{collection.name}
+									</li>
+								</Link>
+							)
+						})
+					}
 				</ul>
 			</div>
 		</nav>
